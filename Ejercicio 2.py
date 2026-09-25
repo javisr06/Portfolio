f2b281b106a50cc0ddb6891c8437f4330a2d1b12
javisr06 <jvrsnchzrz@gmail.com>
2026-09-25 12:35:43 +0000

    Ejercicio 2

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -3,4 +3,8 @@ text="hello world"
 values_int=10
 values_bool= True
 values_nonetype= None
-print(f"{values_float}|{text}|{values_int}|{values_bool}|{values_nonetype}")
+print(f"{values_float}      {type(values_float)}")
+print(f"{text}        {type(text)}")
+print(f"{values_int}        {type(values_int)}")
+print(f"{values_bool}       {type(values_bool)}")
+print(f"{values_nonetype}   {type(values_nonetype)}")
